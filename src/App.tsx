49e7ca24140ac6493662f60122bec18598f65da9
Pablo <pablo.c@westlandreg.com>
2024-05-31 07:26:49 -0400

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import { LandingPage } from './landing-page/LandingPage';
 
 function App() {
   return (
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LandingPage />} />
+          <Route path="test" element={<div>test</div>} />
+          <Route path="*" element={<div>no page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
